e908a6949159d8b47ac6be8cb316dedc1ca285de
sarihahope <sarihaslink@yahoo.com>
2025-01-31 19:14:54 -0800

    working on validation

--- helpers/validate.js
+++ helpers/validate.js
@@ -1,9 +1,24 @@
-const Validator = require('validator');
+// const Validator = require('validator.js');
+// const validator = (body, rules, customMessages, callback) => {
+//   const validation = new Validator(body, rules, customMessages);
+//   validation.passes(() => callback(null, true));
+//   validation.fails(() => callback(validation.errors, false));
+// };
+
+// module.exports = validator;
+
+const validate = require('validate.js');
+
 const validator = (body, rules, customMessages, callback) => {
-  const validation = new Validator(body, rules, customMessages);
-  validation.passes(() => callback(null, true));
-  validation.fails(() => callback(validation.errors, false));
+  const validationResults = validate(body, rules, customMessages);
+  
+  if (validationResults) {
+    // Validation failed
+    callback(validationResults, false);
+  } else {
+    // Validation passed
+    callback(null, true);
+  }
 };
 
 module.exports = validator;
-
