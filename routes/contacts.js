3bc15f08915ecc9b0995e5996d3a5a77009a7f6f
sarihahope <sarihaslink@yahoo.com>
2025-02-01 20:43:44 -0800

    Remove extra not used code

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,17 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const contactsController = require('../controllers/contacts');
-// const validation = require('../middleware/validate');
-// const { validateContact, validatePeople } = require('../middleware/validate');
-
-// router.get('/', contactsController.getContacts);
-// router.get('/:id', contactsController.getSingle);
-// router.post('/', validation.saveContact, isAuthenticated, contactsController.createContact);
-// router.put('/:id', validation.saveContact, isAuthenticated, contactsController.updateContact);
-// router.delete('/:id', isAuthenticated, contactsController.deleteContact);
-
-
-// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const contactsController = require('../controllers/contacts');
--- routes/people.js
+++ routes/people.js
@@ -1,17 +1,4 @@
-// const express = require('express');
-// const router = express.Router();
-// const { validatePeople } = require('../middleware/validate');
-// const peopleController = require('../controllers/people');
-// const {isAuthenticated} = require('../middleware/authenticate');
 
-// router.get('/', peopleController.getPeople);
-// router.get('/:id', peopleController.getSingle);
-// router.post('/', validation.savePeople, isAuthenticated, peopleController.addPerson);
-// router.put('/:id', validation.savePeople, isAuthenticated, peopleController.updatePerson);
-// router.delete('/:id', isAuthenticated , peopleController.deletePerson);
-
-
-// module.exports = router;
 const express = require('express');
 const router = express.Router();
 // const { validatePeople } = require('../middleware/validate'); 
